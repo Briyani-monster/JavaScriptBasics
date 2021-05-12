6a11d90fde808ea13cdaaf6268b92dec9c3db108
briyani-monster <ursashish1999@gmail.com>
2021-05-12 10:39:09 +0530

    adding premitive

---
+++ Basics/PrimitiveTypes.js
@@ -0,0 +1,22 @@
+// Two Types of variable Types
+// 1. Primitive type
+// 2. dynamic type
+
+// primitive type
+// String ,Number Boolean undefined, null ,symbol in Es6
+
+let Name="Mosh";// String literals
+console.log("Type of Name variable: "+typeof Name);//Checking type
+
+let number=2;//number literals
+console.log("Type of Number variable: "+typeof number);//Checking type
+
+let isName=true;// boolean literals
+console.log("Type of isName variable: "+typeof isName);//Checking type
+
+let firstName;// undefined literals
+console.log("Type of firstName variable: "+typeof firstName);//Checking type
+
+let lastName=null;// is used to explicitly clear variable value
+console.log("Type of lastName variable: "+typeof lastName);//Checking type
+
