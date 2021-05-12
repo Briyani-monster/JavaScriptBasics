a8e3aa77b8794a643edd43f55c569e2a0a09387f
briyani-monster <ursashish1999@gmail.com>
2021-05-13 00:54:40 +0530

    adding new files

---
+++ DOM Manipulation/Project1GuessMyNumber/script.js
@@ -0,0 +1,6 @@
+//easy way of selecting a data in java script
+console.log(document.querySelector('.message'));//selects complete <p class='message>Start Guessing...</p>
+console.log(document.querySelector('.message').textContent);//return start guessing
+
+//What is DOM manipulation
+//Solution stands for Document Object Model: Structured Representation of HTMl docs. Allows javascript to acess HTML
